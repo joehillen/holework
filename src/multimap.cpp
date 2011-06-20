87f634d471e47caa35a49b697ea39ba73444f2de
Joe Hillenbrand <joehillen@gmail.com>
2011-06-20 00:16:00 -0700

    Added super awesome many-relation multimap

---
+++ src/multimap.cpp
@@ -0,0 +1,119 @@
+// multimap.cpp
+//
+#include "multimap.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+// UNIT TESTS
+
+TEST(MultimapTests, Count)
+{
+    using namespace boostcraft;
+
+    Multimap<std::string, int> map;
+
+    map.add("foo", 1);
+    map.add("foo", 2);
+    map.add("bar", 1);
+    map.add("baz", 5);
+    map.add("banana", 1);
+    map.add("apple", 1);
+    // suppose the programmer really likes pears
+    map.add("pear", 50);
+    map.add("pear", 50);
+    map.add("pear", 50);
+
+    ASSERT_EQ(2, map.count("foo"));
+    ASSERT_EQ(1, map.count("bar"));
+    ASSERT_EQ(1, map.count("baz"));
+    ASSERT_EQ(1, map.count("banana"));
+    ASSERT_EQ(1, map.count("apple"));
+    ASSERT_EQ(1, map.count("pear"));
+    ASSERT_EQ(0, map.count("wibble"));
+
+    ASSERT_EQ(4, map.count(1));
+    ASSERT_EQ(1, map.count(5));
+    ASSERT_EQ(1, map.count(2));
+    ASSERT_EQ(1, map.count(50));
+    ASSERT_EQ(0, map.count(9001));
+}
+
+template<typename T>
+bool operator==(std::set<T> const& a, std::set<T> const& b)
+{
+    foreach(T element, a) {
+        if (b.count(element) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, std::set<T> set)
+{
+    os << "set( ";
+    foreach(T element, set) {
+        os << element << " ";
+    }
+    os << ")";
+    return os;
+}
+
+TEST(MultimapTests, Get)
+{
+    using namespace boostcraft;
+
+    Multimap<std::string, int> map;
+
+    map.add("foo", 100);
+    map.add("foo", 101);
+    map.add("foo", 102);
+    map.add("bar", 100);
+    map.add("baz", 100);
+    map.add("baz", 101);
+
+    ASSERT_EQ((std::set<std::string>{ "foo", "bar", "baz" }), map.get(100));
+    ASSERT_EQ((std::set<int>{ 100, 101, 102 }), map.get("foo"));
+
+    //empty sets
+    ASSERT_EQ(std::set<std::string>(), map.get(190));
+    ASSERT_EQ(std::set<int>(), map.get("pickle"));
+}
+
+TEST(MultimapTests, RemoveAll)
+{
+    using namespace boostcraft;
+
+    Multimap<std::string, int> map;
+
+    map.removeAll("nonexistent");
+    map.removeAll(42152);
+
+    map.add("foo", 100);
+    map.add("foo", 101);
+    map.add("foo", 102);
+    map.add("bar", 100);
+    map.add("baz", 100);
+    map.add("baz", 101);
+
+    ASSERT_EQ(3, map.count("foo"));
+    ASSERT_EQ(3, map.count(100));
+
+    // Removing 100 should decrease foo's relations
+    map.removeAll(100);
+    ASSERT_EQ(2, map.count("foo"));
+    ASSERT_EQ(0, map.count(100));
+
+    // Removing foo should make it go away
+    map.removeAll("foo");
+    ASSERT_EQ(0, map.count("foo"));
+
+    ASSERT_EQ(1, map.count("baz"));
+    map.removeAll("baz");
+    ASSERT_EQ(0, map.count("baz"));
+}
+
---
+++ src/multimap.h
@@ -0,0 +1,102 @@
+// needchunklist.h
+#pragma once
+
+#include "chunk.h"
+#include "player.h"
+#include <memory>
+#include <map>
+#include <set>
+
+
+namespace boostcraft
+{
+    ////
+    // A two-way associative container which allows the representation of a
+    // many-to-many relation between two domains of different types.
+    //
+    template<typename A, typename B>
+    class Multimap
+    {
+    public:
+        // Create a relation between two elements
+        void add(A a, B b)
+        {
+            // Store relation in both indexes
+            index_a[a].insert(b);
+            index_b[b].insert(a);
+        }
+
+        // Remove the relation between two elements
+        void remove(A a, B b)
+        {
+            // Remove relation from both indexes, deleting lists if empty
+            index_a[a].erase(b);
+            if (index_a[a].empty()) {
+                index_a.erase(a);
+            }
+
+            index_b[b].erase(a);
+            if (index_b[b].empty()) {
+                index_b.erase(b);
+            }
+        }
+
+        // Remove all relations for a given element
+        void removeAll(A a)
+        {
+            // Remove all relations (B, a) from the b index
+            for (auto it = index_b.begin(); it != index_b.end(); ++it) {
+                remove(a, it->first);
+            }
+        }
+
+        void removeAll(B b)
+        {
+            // Remove all relations (A, b) from the a index
+            for (auto it = index_a.begin(); it != index_a.end(); ++it) {
+                remove(it->first, b);
+            }
+        }
+
+
+        // Return the number of elements related to a given element
+        size_t count(A a) const
+        {
+            if (index_a.count(a) == 0) {
+                return 0;
+            }
+            return index_a.find(a)->second.size();
+        }
+
+        size_t count(B b) const
+        {
+            if (index_b.count(b) == 0) {
+                return 0;
+            }
+            return index_b.find(b)->second.size();
+        }
+
+        // Return the list of elements associated with a given element
+        std::set<A> get(B b) const
+        {
+            if (index_b.count(b) == 0) {
+                return std::set<A>();
+            }
+            return index_b.find(b)->second;
+        }
+
+        std::set<B> get(A a) const
+        {
+            if (index_a.count(a) == 0) {
+                return std::set<B>();
+            }
+            return index_a.find(a)->second;
+        }
+
+    private:
+        std::map<A, std::set<B>> index_a;
+        std::map<B, std::set<A>> index_b;
+    };
+
+} // namespace boostcraft
+
